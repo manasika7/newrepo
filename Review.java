040945763aea5792ef4c18ae504b1b386b2fbefd
Manasik <manasika7@gmail.com>
2019-09-08 18:07:35 -0500

    Init

---
+++ Review.java
@@ -0,0 +1,27 @@
+package Day08Operators;
+
+public class Review {
+
+	public static void main(String[] args) {
+		int gallon = 1;
+		double Litters = gallon * 3.785;
+		System.out.println(gallon + "gallons equal to " + Litters+ " Litters");
+		double L = 1;
+		double G = L/3.785;
+		System.out.println(L+ " Litters " + G+ " gallons equal to");
+		int x=2;
+		int y= x++;
+		System.out.println(x+" "+y);
+		int x4 =8;
+		int y4 =x4--;
+		System.out.println(y4);
+		System.out.println(x4);
+		System.out.println(x4);
+		System.out.println(y4);
+		System.out.println(x4);
+		System.out.println(x4);
+		System.out.println(x4);
+	}
+
+	
+}
